33de424cae9067a737656de6007f985e361857eb
김동진 <dongjin403@naver.com>
2024-05-08 23:56:41 +0900

    게시판 기능 구현

--- src/main/java/me/dongjkim/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/dongjkim/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.dongjkim.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.dongjkim.springbootdeveloper.domain.Article;
 import me.dongjkim.springbootdeveloper.dto.ArticleListViewResponse;
+import me.dongjkim.springbootdeveloper.dto.ArticleViewResponse;
 import me.dongjkim.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
