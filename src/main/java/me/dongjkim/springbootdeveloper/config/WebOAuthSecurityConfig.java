7e05d1884fa34ee3559c79b71b37226dd05d3cfd
김동진 <dongjin403@naver.com>
2024-06-15 16:30:27 +0900

    oauth

--- src/main/java/me/dongjkim/springbootdeveloper/config/WebOAuthSecurityConfig.java
+++ src/main/java/me/dongjkim/springbootdeveloper/config/WebOAuthSecurityConfig.java
@@ -2,6 +2,7 @@ package me.dongjkim.springbootdeveloper.config;
 
 import lombok.RequiredArgsConstructor;
 import me.dongjkim.springbootdeveloper.config.jwt.TokenProvider;
+import me.dongjkim.springbootdeveloper.config.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
 import me.dongjkim.springbootdeveloper.config.oauth.OAuth2UserCustomService;
 import me.dongjkim.springbootdeveloper.repository.RefreshTokenRepository;
 import me.dongjkim.springbootdeveloper.service.UserService;
@@ -43,29 +44,35 @@ public class WebOAuthSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+        return http //1. 토큰 방식으로 인증을 하기 때문에 기존에 사용하던 폼 로그인, 세션 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //2. 헤더를 확인할 커스텀 필터 추가
                 .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                //3. 토큰 재발급 URL은 인증없이 접근 가능하도록 설정. 나머지 API URL은 인증 필요
                 .authorizeRequests(auth -> auth
                         .requestMatchers(new AntPathRequestMatcher("/api/token")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/**")).authenticated()
                         .anyRequest().permitAll())
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
+                        //4. Authorization 요청과 관련된 상태 저장
                         .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint.authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
                         .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
+                        //5. 인증 성공 시 실행할 핸들러
                         .successHandler(oAuth2SuccessHandler())
                 )
+                //6. /api로 시작하는 url인 경우 401 상태 코드를 반환하도록 예외 처리
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .defaultAuthenticationEntryPointFor(
                                 new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
                                 new AntPathRequestMatcher("/api/**")
                         ))
                 .build();
+
     }
 
 
